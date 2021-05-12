6f0df428f16b1fca11bf6e01bfb3495b6754a3dd
sanamomo <80310407+sanamomo@users.noreply.github.com>
2021-05-12 19:38:47 +0900

    ユーザー詳細ページ投稿一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # @posts = @user.posts
+    @posts = Post.where(user_id: @user.id)
   end
 
   def update
